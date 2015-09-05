fb0919dba224cfce86b50c27d0dea0bd35b8eb55
wangjiezhe <wangjiezhe@gmail.com>
2015-09-05 15:21:26 +0800

    Add code for Java入门第三季-1

---
+++ SimulationLibrarySystem/src/com/imooc/Books.java
@@ -0,0 +1,11 @@
+package com.imooc;
+
+public class Books {
+	private String[] books = {
+			"数据结构", "高数", "设计模式", "Java"
+	};
+
+	public String[] getBooks() {
+		return books;
+	}
+}
---
+++ SimulationLibrarySystem/src/com/imooc/Borrow.java
@@ -0,0 +1,84 @@
+package com.imooc;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Borrow {
+	static String[] books = (new Books()).getBooks();
+
+	@SuppressWarnings("serial")
+	public static class BookNotExistException extends Exception {
+		public BookNotExistException() {
+			System.out.println("图书不存在!");
+		}
+	}
+
+	public static void main(String[] args) {
+//		for (String book:books) {
+//			System.out.println(book);
+//		}
+		Scanner sc = new Scanner(System.in);
+		int method;
+		String result;
+		while (true) {
+			while (true) {
+				try {
+					System.out.println("输入命令： 1-按照名称查找图书; 2-按照序号查找图书");
+					method = sc.nextInt();
+					if (method == 1 || method == 2) {
+						break;
+					} else {
+						throw new InputMismatchException();
+					}
+				} catch (InputMismatchException e) {
+					System.out.println("命令输入错误！请根据提示输入数字命令！");
+					sc.nextLine();
+				}
+			}
+//			System.out.println(method);
+			try {
+				if (method == 2) {
+					int id;
+					while (true) {
+						try {
+							System.out.println("输入图书序号：");
+							id = sc.nextInt();
+							break;
+						} catch (InputMismatchException e) {
+							System.out.println("命令输入错误！请根据提示输入数字命令！");
+							sc.nextLine();
+						}
+					}
+					result = getBookById(id);
+				} else {
+					System.out.println("输入图书名称");
+					String name = sc.next();
+					System.out.println(name);
+					result = getBookByName(name);
+				}
+				break;
+			} catch (BookNotExistException e) {
+				sc.nextLine();
+			}
+		}
+		System.out.println("book: "+result);
+		sc.close();
+	}
+
+	public static String getBookById(int id) throws BookNotExistException {
+		try {
+			return books[id-1];
+		} catch (ArrayIndexOutOfBoundsException e) {
+			throw new BookNotExistException();
+		}
+	}
+	
+	public static String getBookByName(String name) throws BookNotExistException {
+		for (String book:books) {
+			if (name.equals(book)) {
+				return name;
+			}
+		}
+		throw new BookNotExistException();
+	}
+}
